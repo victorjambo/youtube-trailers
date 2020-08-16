24a5f5fbf1a3f61684edf8ad26b9e25c4c420e88
victorjambo <victorjambo@live.com>
2020-08-17 02:07:16 +0300

    feat: it works

---
+++ background.js
@@ -0,0 +1,29 @@
+
+var menuItem = {
+  "id": "YouTube",
+  "title": "Trailer on YouTube",
+  "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+function slugify(text) {
+  return text
+    .toString()
+    .normalize('NFD')
+    .trim()
+    .replace(/\s+/g, '+');
+}
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+  if (clickData.menuItemId == "YouTube" && clickData.selectionText) {
+    var url = "https://www.youtube.com/results?search_query=" + slugify(clickData.selectionText) + "+trailer";
+    var createData = {
+      "url": url,
+      "active": true
+    };
+    chrome.tabs.create(createData, function () { });
+  }
+});
+
+// https://www.youtube.com/results?search_query=hello+world
--- content.js
+++ content.js
@@ -1,11 +1,46 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/**
+ * Responsible for
+ * 1. Adding the youtube button to the page
+ * 2. Show the modal
+ */
 
-'use strict';
+var $trailer = $(`
+  <a class="btn movie-info-start-watch-btn" id="trailer" data-toggle="modal" data-src="https://www.youtube.com/embed/IP7uGKgJL8U" data-target="#myModal">
+    <div class="play-arrow-container">
+      <div class="play-arrow"></div>
+    </div>
+    TRAILER
+  </a>
+`)
+$(".row-fluid:contains('START')").append($trailer);
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
+var $modal = $(`
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-body">
+          <button type="button" class="close" id="close-modal" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <!-- 16:9 aspect ratio -->
+          <div class="embed-responsive embed-responsive-16by9" id="embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always" allow="autoplay"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+`);
+$("body").append($modal);
+
+$('#trailer').click(function() {
+    var $videoSrc = $(this).data("src");
+    $('#myModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" );
+    })
 });
+
+// stop playing the youtube video when I close the modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    $("#video").attr('src', '');
+})
